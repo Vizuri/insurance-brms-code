40aa8eb96103896a137080266ca8839fc1f1d250
ashakya <ashakya@vizuri.com>
2015-06-18 17:52:46 -0400

    test clean up

--- insurance-web-app/src/test/java/com/vizuri/insurance/test/rules/TestUtils.java
+++ insurance-web-app/src/test/java/com/vizuri/insurance/test/rules/TestUtils.java
@@ -21,7 +21,7 @@ import com.vizuri.insurance.rest.TransferWrapper;
 import com.vizuri.insurance.rest.brms.RuleProcessor;
 
 public class TestUtils {
-	
+	private static TestUtils u = new TestUtils();
 	public static void setUpProps(){
 		//Dkie.maven.settings.custom={custom.settings.location.full.path}
 		System.setProperty("kie.maven.settings.custom", "/Users/ashakya/insurance/m2repo/mac-settings.xml");
@@ -49,12 +49,12 @@ public class TestUtils {
 	public static Object fromJson(String fileName,Class clazz){
 		
 		if(! fileName.startsWith("/")){
-			fileName += "/"+fileName;
+			//fileName += "/"+fileName;
 		}
 		RuleProcessor rp = new RuleProcessor();
 		
 		//System.out.println(testUrl);
-		InputStream fios = TestUtils.class.getResourceAsStream(fileName);//
+		InputStream fios = rp.getClass().getResourceAsStream(fileName);//
 		
 		//new FileInputStream("/Users/ashakya/insurance/workspace/vizuri_brms-insurance-code/insurance-web-app/src/main/resources/test.js");
 		InputStreamReader fread = new InputStreamReader(fios);
